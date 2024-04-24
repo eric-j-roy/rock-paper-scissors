82c7160e89ff4a7e45720a26e4dd7ab3261156bc
eric-j-roy <119775919+eric-j-roy@users.noreply.github.com>
2024-04-23 20:23:57 -0400

    Added pseudocode

---
+++ javascript.js
@@ -0,0 +1,7 @@
+// User inputs selection of rock, paper, or scissors
+
+// A random selection is chosen to play against the user's selection
+
+// Determine winner of the round based on selections
+
+// Track the score of the game until one side has 5 wins
