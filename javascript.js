66d7d2ee49b9ae8025daa7869f6fc9509f59c5de
eric-j-roy <119775919+eric-j-roy@users.noreply.github.com>
2024-04-23 21:25:07 -0400

    Added getHumanChoice function

--- javascript.js
+++ javascript.js
@@ -1,22 +1,29 @@
 // User inputs selection of rock, paper, or scissors
+function getHumanChoice(humanChoice) {
+    humanChoice = prompt("Select your weapon of choice - ROCK, PAPER, or SCISSORS: ");
+    humanChoice = humanChoice.toUpperCase();
+
+    return humanChoice;
+}
+
+console.log(getHumanChoice());
 
 // A random selection is chosen to play against the user's selection
-
-// Determine winner of the round based on selections
-
-// Track the score of the game until one side has 5 wins
-
 function getComputerChoice(computerChoice) {
     let randomNumber = Math.random();
     if (randomNumber >= 0.67) {
-        computerChoice = "Rock";
+        computerChoice = "ROCK";
     }   else if (randomNumber >= 0.33) {
-        computerChoice = "Paper";
+        computerChoice = "PAPER";
     }   else {
-        computerChoice = "Scissors";
+        computerChoice = "SCISSORS";
     }
-    console.log(randomNumber);
+
     return computerChoice;
     }
 
 console.log(getComputerChoice("Test"));
+
+// Determine winner of the round based on selections
+
+// Track the score of the game until one side has 5 wins
