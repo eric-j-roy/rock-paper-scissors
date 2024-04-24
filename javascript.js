ba90142ac635e7c1a8bd9689375fc594bf8e6736
eric-j-roy <119775919+eric-j-roy@users.noreply.github.com>
2024-04-23 21:00:47 -0400

    Added getComputerChoice function

--- javascript.js
+++ javascript.js
@@ -5,3 +5,18 @@
 // Determine winner of the round based on selections
 
 // Track the score of the game until one side has 5 wins
+
+function getComputerChoice(computerChoice) {
+    let randomNumber = Math.random();
+    if (randomNumber >= 0.67) {
+        computerChoice = "Rock";
+    }   else if (randomNumber >= 0.33) {
+        computerChoice = "Paper";
+    }   else {
+        computerChoice = "Scissors";
+    }
+    console.log(randomNumber);
+    return computerChoice;
+    }
+
+console.log(getComputerChoice("Test"));
